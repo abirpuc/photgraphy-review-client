ca3e81ce108f0e31c7c53149b35ee9b234096e38
abirpuc <mhabirbd2016@gmail.com>
2022-11-20 21:18:25 +0600

    environment vriable setup and a simple navbar layout setup

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnK2B8fRW5cVBPM9WlUQ09htqw-nkV60Y",
-  authDomain: "photography-review-fdda3.firebaseapp.com",
-  projectId: "photography-review-fdda3",
-  storageBucket: "photography-review-fdda3.appspot.com",
-  messagingSenderId: "473969305687",
-  appId: "1:473969305687:web:b0cad8cf14814bc010db12",
-  measurementId: "G-7BPSC8J8HG"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
