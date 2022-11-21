854efa7fd3cf0149423bb12fc244a41500d131b7
abirpuc <mhabirbd2016@gmail.com>
2022-11-21 19:30:26 +0600

    create all services page

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ServiceItem from './ServiceItem';
+import ServiceItem from '../Home/Services/ServiceItem';
 
 const Services = () => {
     const [services, setServices] = useState([])
@@ -11,22 +10,16 @@ const Services = () => {
             .then(data => setServices(data))
     }, [])
     return (
-        <div className='mt-20'>
-            <div className='text-center font-bold'>
-                <h1 className='text-2xl text-primary'>Our Services</h1>
-            </div>
+        <div className='w-3/4 mx-auto'>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4'>
+
                 {
                     services.map(service => <ServiceItem
                         key={service._id}
                         service={service}
                     ></ServiceItem>)
                 }
-            </div>
-            <div className='text-center my-6'>
-                <Link to="/services">
-                    <button className='btn btn-outline btn-primary'>See More</button>
-                </Link>
+
             </div>
         </div>
     );
